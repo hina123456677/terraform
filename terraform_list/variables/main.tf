0380f4933126b03657a10e0562fcb02d3268ae01
hina123456677 <hbatool551@gmail.com>
2025-12-30 16:40:08 +0500

    list

---
+++ terraform_list/variables/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "apache_sg" {
+  name        = var.sg_name
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  # Single static egress rule
+  egress {
+    from_port   = var.egress_port
+    to_port     = var.egress_port
+    protocol    = var.egress_protocol
+    cidr_blocks = var.cidr_blocks
+  }
+
+  tags = {
+    Name = var.sg_name
+  }
+}
+
+resource "aws_instance" "apache_server" {
+  ami           = var.ami_id
+  count         = var.instance_count
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.apache_sg.id]
+
+  tags = {
+    Name = var.instance_name[count.index]
+  }
+}
+
+output "ec2_public_ip" {
+  value       = aws_instance.apache_server[*].public_ip
+  description = "Public IPs of Apache servers"
+}
---
+++ terraform_list/variables/variables.tf
@@ -0,0 +1,67 @@
+# AWS region
+variable "region" {
+  description = "AWS region to create resources in"
+  type        = string
+}
+
+# AMI ID for EC2 instance
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+}
+
+# EC2 instance type
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
+
+variable "instance_name"{
+  description = "name of instance"
+  type        =         list(string)
+}
+
+# Key pair name for SSH access
+variable "key_name" {
+  description = "Key pair name to access EC2 instance"
+  type        = string
+}
+
+# Security Group Name
+variable "sg_name" {
+  description = "Security group name"
+  type        = string
+}
+
+# Allowed CIDR blocks
+variable "cidr_blocks" {
+  description = "List of allowed CIDR blocks"
+  type        = list(string)
+}
+
+# Ports for ingress rules
+variable "ssh_port" {
+  description = "SSH port"
+  type        = number
+}
+
+variable "http_port" {
+  description = "HTTP port"
+  type        = number
+}
+
+variable "egress_port" {
+  description = "Egress from and to port (set 0 for all ports)"
+  type        = number
+}
+
+variable "egress_protocol" {
+  description = "Egress protocol"
+  type        = string
+}
+
+# Number of EC2 instances
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
