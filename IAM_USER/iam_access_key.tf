911cb927ef9b05dba7a24deb9967fa52cd3be389
hina123456677 <hbatool551@gmail.com>
2026-01-08 17:41:48 +0500

    file

--- IAM_USER/iam_access_key.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_iam_access_key" "test_user_key" {
-  user = aws_iam_user.test_user.name
-}
--- IAM_USER/s3.tf
+++ IAM_USER/s3.tf
@@ -4,4 +4,4 @@ resource "aws_s3_bucket" "test_bucket" {
   tags = {
     Name = "TestBucket"
   }
-}
+}  
